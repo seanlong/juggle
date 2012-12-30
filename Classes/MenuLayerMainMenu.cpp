82c06ebe4c2f62f57b31aef8216d71c3e3859682
xuchao1129 <xuchao1129@163.com>
2012-12-30 11:47:42 +0800

    add two files

---
+++ Classes/MenuLayerMainMenu.cpp
@@ -0,0 +1,113 @@
+#include "MenuLayerMainMenu.h"
+
+#include "HelloWorldScene.h"
+
+//------------------------------------------------------------------
+//
+// MenuLayerMainMenu
+//
+//------------------------------------------------------------------
+MenuLayerMainMenu::MenuLayerMainMenu()
+{
+
+}
+bool MenuLayerMainMenu::init()
+{
+  if ( !CCLayer::init() )
+  {
+      return false;
+  }
+    setTouchEnabled(true);
+    setTouchPriority(kCCMenuHandlerPriority + 1);
+    setTouchMode(kCCTouchesOneByOne);
+
+
+
+  
+    
+    // simple Item
+    CCMenuItemFont *item1 = CCMenuItemFont::create("simple", this, menu_selector(MenuLayerMainMenu::menuCallbackStartGame) );
+    
+
+    // hard Item
+    CCMenuItemFont::setFontName("Marker Felt");
+    CCMenuItemFont *item2 = CCMenuItemFont::create("hard", this, menu_selector(MenuLayerMainMenu::menuCallbackStartGame));
+    
+    // tough Item
+    CCMenuItemFont *item3 = CCMenuItemFont::create("tough", this, menu_selector(MenuLayerMainMenu::menuCallbackStartGame));
+
+    // exit Item
+    CCMenuItemFont* item4 = CCMenuItemFont::create("Quit", this, menu_selector(MenuLayerMainMenu::onQuit));
+    
+     CCMenu* menu = CCMenu::create( item1, item2, item3, item4, NULL);
+    menu->alignItemsVertically();
+    
+    
+    // elastic effect
+    CCSize s = CCDirector::sharedDirector()->getWinSize();
+    
+    addChild(menu);
+    menu->setPosition(ccp(s.width/2, s.height/2));
+	return true;
+}
+
+bool MenuLayerMainMenu::ccTouchBegan(CCTouch *touch, CCEvent * pEvent)
+{
+    return true;
+}
+
+void MenuLayerMainMenu::ccTouchEnded(CCTouch *touch, CCEvent * pEvent)
+{
+}
+
+void MenuLayerMainMenu::ccTouchCancelled(CCTouch *touch, CCEvent * pEvent)
+{
+}
+
+void MenuLayerMainMenu::ccTouchMoved(CCTouch *touch, CCEvent * pEvent)
+{
+}
+
+MenuLayerMainMenu::~MenuLayerMainMenu()
+{
+}
+
+void MenuLayerMainMenu::menuCallbackStartGame(CCObject* sender)
+{
+    // create a scene. it's an autorelease object
+    CCScene *pScene = HelloWorld::scene();
+
+    // run
+    CCDirector::sharedDirector()->replaceScene(pScene);
+	pScene->release();
+}
+
+void MenuLayerMainMenu::allowTouches(float dt)
+{
+    CCDirector* pDirector = CCDirector::sharedDirector();
+    pDirector->getTouchDispatcher()->setPriority(kCCMenuHandlerPriority+1, this);
+    unscheduleAllSelectors();
+    CCLog("TOUCHES ALLOWED AGAIN");
+}
+
+
+
+void MenuLayerMainMenu::onQuit(CCObject* sender)
+{
+
+
+
+   CCDirector::sharedDirector()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+
+CCScene* MenuLayerMainMenu::scene()
+{
+  CCScene *scene = CCScene::create();
+  MenuLayerMainMenu *layer = MenuLayerMainMenu::create();
+  scene->addChild(layer);
+  return scene;
+}
---
+++ Classes/MenuLayerMainMenu.h
@@ -0,0 +1,29 @@
+#ifndef __SETTINGS_MENU_H__
+#define __SETTINGS_MENU_H__ 1
+#include "cocos2d.h"
+
+using namespace cocos2d;
+
+class MenuLayerMainMenu : public CCLayer
+{
+
+
+public:
+	static cocos2d::CCScene* scene();
+    MenuLayerMainMenu(void);
+    ~MenuLayerMainMenu();
+
+public:
+	virtual bool init();
+    virtual bool ccTouchBegan(CCTouch *touch, CCEvent * pEvent);
+    virtual void ccTouchEnded(CCTouch *touch, CCEvent * pEvent);
+    virtual void ccTouchCancelled(CCTouch *touch, CCEvent * pEvent);
+    virtual void ccTouchMoved(CCTouch *touch, CCEvent * pEvent);
+
+    void allowTouches(float dt);
+    void menuCallbackStartGame(CCObject* pSender);
+    void onQuit(CCObject* pSender);
+	CREATE_FUNC(MenuLayerMainMenu);
+};
+
+#endif
